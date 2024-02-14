e87f6a51de5fe3450786ef88612d7da3e74116f2
Viswadeep Mallarapu Bhaskar <viswadeep@Viswadeeps-MacBook-Pro.local>
2024-02-13 23:32:55 -0500

    updated

--- all_questions.py
+++ all_questions.py
@@ -248,7 +248,7 @@ def question7():
     answer = {}
 
     # float
-    answer["a, info gain, ID"] = 1.
+    answer["a, info gain, ID"] = 1.0
     answer["b, info gain, Handedness"] = 0.531
 
     # string: "ID" or "Handedness"
