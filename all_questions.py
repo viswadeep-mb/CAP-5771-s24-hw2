4f6f9303f1aa862e5e98d45217c7effc89a0b1de
Viswadeep Mallarapu Bhaskar <viswadeep@Viswadeeps-MacBook-Pro.local>
2024-02-13 23:07:15 -0500

    updated answers

--- all_questions.py
+++ all_questions.py
@@ -23,52 +23,60 @@ def question1():
     level2_left = {}
     level2_right = {}
 
-    level1["smoking"] = 0.
-    level1["smoking_info_gain"] = 0.
+    level1["smoking"] = 1.
+    level1["smoking_info_gain"] = 0.2781
 
-    level1["cough"] = 0.
-    level1["cough_info_gain"] = 0.
+    level1["cough"] = -1.
+    level1["cough_info_gain"] = 0.2365
 
-    level1["radon"] = 0.
-    level1["radon_info_gain"] = 0.
+    level1["radon"] = -1.
+    level1["radon_info_gain"] = 0.0350
 
-    level1["weight_loss"] = 0.0
-    level1["weight_loss_info_gain"] = 0.
+    level1["weight_loss"] = -1.
+    level1["weight_loss_info_gain"] = 0.0291
 
-    level2_left["smoking"] = 0.
+    level2_left["smoking"] = -1.
     level2_left["smoking_info_gain"] = 0.
-    level2_right["smoking"] = 0.
+    level2_right["smoking"] = -1.
     level2_right["smoking_info_gain"] = 0.
 
-    level2_left["radon"] = 0.
-    level2_left["radon_info_gain"] = 0.
+    level2_left["radon"] = -1.
+    level2_left["radon_info_gain"] = 0.0729
 
-    level2_left["cough"] = 0.
-    level2_left["cough_info_gain"] = 0.
+    level2_left["cough"] = 1.
+    level2_left["cough_info_gain"] = 0.7219
 
-    level2_left["weight_loss"] = 0.
-    level2_left["weight_loss_info_gain"] = 0.
+    level2_left["weight_loss"] = -1.
+    level2_left["weight_loss_info_gain"] = 0.1711
 
-    level2_right["radon"] = 0.
-    level2_right["radon_info_gain"] = 0.
+    
+    level2_right["radon"] = 1.
+    level2_right["radon_info_gain"] = 0.7219
 
-    level2_right["cough"] = 0.
-    level2_right["cough_info_gain"] = 0.
+    level2_right["cough"] = -1.
+    level2_right["cough_info_gain"] = 0.3219
 
-    level2_right["weight_loss"] = 0.
-    level2_right["weight_loss_info_gain"] = 0.
+    level2_right["weight_loss"] = -1.
+    level2_right["weight_loss_info_gain"] = 0.1711
 
     answer["level1"] = level1
     answer["level2_left"] = level2_left
     answer["level2_right"] = level2_right
 
-    # Fill up `construct_tree``
-    # tree, training_error = construct_tree()
-    tree = u.BinaryTree("root")  # MUST STILL CREATE THE TREE *****
-    answer["tree"] = tree  # use the Tree structure
-    # answer["training_error"] = training_error
-    answer["training_error"] = 0.0  
 
+    tree=u.BinaryTree("smoking")
+    tree.insert_left("cough")
+    tree.insert_right("radon")
+    tree.left.insert_left("yes")
+    tree.left.insert_right("no")
+    tree.right.insert_left("yes")
+    tree.right.insert_right("no")
+    answer["tree"] = tree  
+
+    
+    # answer["training_error"] = num_of_misclassifications/total_records = 0/10=0
+    
+    answer["training_error"] = 0.0  
     return answer
 
 
@@ -79,14 +87,42 @@ def question2():
     answer = {}
 
     # Answers are floats
-    answer["(a) entropy_entire_data"] = 0.
+    '''
+    Area_A=0.41
+    Area_B=0.46
+    Area_C=0.13
+    ent_total=-(Area_A*u.log2(Area_A) + Area_B*u.log2(Area_B)+ Area_C*u.log2(Area_C) )
+    '''
+    answer["(a) entropy_entire_data"] = 1.425
+    
     # Infogain
-    answer["(b) x <= 0.2"] = 0.
-    answer["(b) x <= 0.7"] = 0.
-    answer["(b) y <= 0.6"] = 0.
+    '''
+    Area_A=0
+    Area_B=0.16
+    Area_C=0.04
+    ent_x_2=-(Area_A*u.log2(Area_A) + Area_B*u.log2(Area_B)+ Area_C*u.log2(Area_C) )
+    '''
+    answer["(b) x <= 0.2"] = 0.608
+
+    '''
+    Area_A=0.20
+    Area_B=0.46
+    Area_C=0.04
+    ent_x_7=-(Area_A*u.log2(Area_A) + Area_B*u.log2(Area_B)+ Area_C*u.log2(Area_C) )
+    '''
+    answer["(b) x <= 0.7"] = 1.165
+
+    '''
+    Area_A=0.09
+    Area_B=0.42
+    Area_C=0.09
+    ent_y_6=-(Area_A*u.log2(Area_A) + Area_B*u.log2(Area_B)+ Area_C*u.log2(Area_C) )
+    '''
+    answer["(b) y <= 0.6"] = 1.150
 
     # choose one of 'x=0.2', 'x=0.7', or 'x=0.6'
-    answer["(c) attribute"] = ""  
+    '''Entropy of x<=0.2 is less compared to other splits'''
+    answer["(c) attribute"] = "x<=0.2"  
 
     # Use the Binary Tree structure to construct the tree
     # Answer is an instance of BinaryTree
@@ -103,18 +139,18 @@ def question3():
     answer = {}
 
     # float
-    answer["(a) Gini, overall"] = 0.
+    answer["(a) Gini, overall"] = 0.5
 
     # float
     answer["(b) Gini, ID"] = 0.0
-    answer["(c) Gini, Gender"] = 0.
-    answer["(d) Gini, Car type"] = 0.
-    answer["(e) Gini, Shirt type"] = 0.
+    answer["(c) Gini, Gender"] = 0.48
+    answer["(d) Gini, Car type"] = 0.1622
+    answer["(e) Gini, Shirt type"] = 0.4914
 
-    answer["(f) attr for splitting"] = ""
+    answer["(f) attr for splitting"] = "Car type"
 
     # Explanatory text string
-    answer["(f) explain choice"] = ""
+    answer["(f) explain choice"] = "Car type has the lowest gini among the three attributes(Gender,Car type and Shirt)"
 
     return answer
 
@@ -135,45 +171,45 @@ def question4():
     #  'quantitative', 'interval', 'ratio'
     # If you have a choice between 'binary' and 'discrete', choose 'binary'
 
-    answer["a"] = []
+    answer["a"] = ['binary','qualitative','ordinal']
 
     # Explain if there is more than one interpretation. Repeat for the other questions. At least five words that form a sentence.
-    answer["a: explain"] = ""
+    answer["a: explain"] = "Considering Time in AM/PM as Binary"
 
-    answer["b"] = []
+    answer["b"] = ['continuous','quantitative','ratio']
     answer["b: explain"] = ""
 
-    answer["c"] = []
+    answer["c"] = ['discrete','qualitative','ordinal']
     answer["c: explain"] = ""
 
-    answer["d"] = []
+    answer["d"] = ['continuous','quantitative','ratio']
     answer["d: explain"] = ""
 
-    answer["e"] = []
+    answer["e"] = ['discrete','qualitative','ordinal']
     answer["e: explain"] = ""
 
-    answer["f"] = []
-    answer["f: explain"] = ""
+    answer["f"] = ['continuous','quantitative','interval']
+    answer["f: explain"] = "If sea level is not considered as an arbitrary origin,It can also be ratio"
 
-    answer["g"] = []
+    answer["g"] = ['discrete','quantitative','ratio']
     answer["g: explain"] = ""
 
-    answer["h"] = []
+    answer["h"] = ['discrete','qualitative','nominal']
     answer["h: explain"] = ""
 
-    answer["i"] = []
+    answer["i"] = ['discrete','qualitative','ordinal']
     answer["i: explain"] = ""
 
-    answer["j"] = []
+    answer["j"] = ['discrete','qualitative','ordinal']
     answer["j: explain"] = ""
 
-    answer["k"] = []
-    answer["k: explain"] = ""
+    answer["k"] = ['continuous','quantitative','ratio']
+    answer["k: explain"] = "It can be interval attribute as well if distance of 0 from the center of campus is considered as an arbitrary origin"
 
-    answer["l"] = []
+    answer["l"] = ['continuous','quantitative','ratio']
     answer["l: explain"] = ""
 
-    answer["m"] = []
+    answer["m"] = ['discrete','qualitative','nominal']
     answer["m: explain"] = ""
 
     return answer
@@ -188,18 +224,18 @@ def question5():
     # Read appropriate section of book chapter 3
 
     # string: one of 'Model 1' or 'Model 2'
-    explain["a"] = ""
-    explain["a explain"] = ""
+    explain["a"] = "Model 2"
+    explain["a explain"] = "Model 1 has high accuracy on A but accuracy drops on B which indicates overfitting, But for Model 2 accuracy on both Dataset A and B are almost same indicating better generalization to new data. "
 
     # string: one of 'Model 1' or 'Model 2'
-    explain["b"] = ""
-    explain["b explain"] = ""
+    explain["b"] = "Model 2"
+    explain["b explain"] = "Though accuracy drops slightly Model 2 is still preferable due to better generalization observed earlier and it is less likely to overfit."
 
-    explain["c similarity"] = ""
-    explain["c similarity explain"] = ""
+    explain["c similarity"] = "Regularization"
+    explain["c similarity explain"] = "Both techniques aims to reduce overfitting by penalizing models for complexity."
 
-    explain["c difference"] = ""
-    explain["c difference explain"] = ""
+    explain["c difference"] = "Specificity"
+    explain["c difference explain"] = "MDL aims for a model that requires fewer bits to describe,whereas pessimistic error aims to adjust tree error to avoid complex decision tree.
 
     return explain
 
@@ -238,19 +274,19 @@ def question7():
     answer = {}
 
     # float
-    answer["a, info gain, ID"] = 0.
-    answer["b, info gain, Handedness"] = 0.
+    answer["a, info gain, ID"] = 1.
+    answer["b, info gain, Handedness"] = 0.531
 
     # string: "ID" or "Handedness"
-    answer["c, which attrib"] = ""
+    answer["c, which attrib"] = "ID"
 
     # answer is a float
-    answer["d, gain ratio, ID"] = 0.
-    answer["e, gain ratio, Handedness"] = 0.
+    answer["d, gain ratio, ID"] = 0.231
+    answer["e, gain ratio, Handedness"] = 0.531
 
     # string: one of 'ID' or 'Handedness' based on gain ratio
     # choose the attribute with the largest gain ratio
-    answer["f, which attrib"] = ""
+    answer["f, which attrib"] = "Handedness"
 
     return answer
 
@@ -268,4 +304,4 @@ if __name__ == "__main__":
     answers["q7"] = question7()
 
     u.save_dict("answers.pkl", answers)
-
+    #print(u.load_dict("answers.pkl",answers))
